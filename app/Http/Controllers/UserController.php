46ba3587a5e9ff6b216813659628979f132a164a
vagrant <vagrant@homestead>
2023-10-16 21:01:40 +0000

    Controlador: b.En esta misma consulta también necesito retornar una posición mas llamada edad, la cual me calculará la edad del usuario en base a la fecha de nacimiento.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use DateTime;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     function all() {
         $users = User::with('domicilio')->get();
+        
+        foreach ($users as $user){
+            $cumple = new DateTime($user->fecha_nacimiento);
+            $hoy = new DateTime();
+            $edad = $hoy->diff($cumple);
+            $user->edad = $edad->y;
+        }
 
         return $users; 
     }
